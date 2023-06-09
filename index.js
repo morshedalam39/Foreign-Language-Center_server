b00f4300b078a5e6cb649899ddba91254e12e905
morshedalam39 <morshedalam4239@gmail.com>
2023-06-09 11:01:54 +0600

    user role api done

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bthwvgc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,55 +17,79 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
+  },
+});
+
+
+
+const dbConnect = () => {
+  try {
+    client.connect();
+    console.log(" Database Connected Successfully✅ ");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
+
+
+
+const usersCollection = client.db("21-Language").collection("users");
+const classesCollection = client.db("21-Language").collection("classes");
+
+app.get("/", (req, res) => {
+  res.send("Learn Language");
+});
+
+// users related apis
+app.get("/users", async (req, res) => {
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+});
+
+app.get("/role", async (req, res) => {
+  const email = req.query?.email;
+  console.log(email);
+  const role = await usersCollection.findOne({ email: email });
+  if (role) {
+    res.send(role);
+  } else {
+    res.send({});
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  console.log(user);
+  const existingUser = await usersCollection.findOne(query);
 
-
-    const usersCollection = client.db("21-Language").collection("users");
-
-
-      // users related apis
-    //   app.get('/users', async (req, res) => {
-    //     const result = await usersCollection.find().toArray();
-    //     res.send(result);
-    //   });
-  
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query);
-  
-        if (existingUser) {
-          return res.send({ message: 'user already exists' })
-        }
-  
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  if (existingUser) {
+    return res.send({ message: "user already exists" });
   }
-}
-run().catch(console.dir);
+
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const quary = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updateDoc = {
+    $set: {
+      role: body.role,
+    },
+  };
+  const result = await usersCollection.updateOne(quary, updateDoc, options);
+  res.send(result);
+});
 
 
 
 
-
-app.get('/', (req, res) => {
-    res.send('Learn Language')
-})
 
 app.listen(port, () => {
-    console.log(`Learn Language is Process on ${port}`);
-})
+  console.log(`Learn Language is Process on ${port}`);
+});
